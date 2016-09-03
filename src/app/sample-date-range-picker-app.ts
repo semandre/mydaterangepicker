f5819befdac8fbcf8daab4d52132d5ad14c00e2e
kekeh <kea@live.fi>
2016-09-03 15:59:06 +0300

    Demo page updated to use Angular 2 RC 6

--- src/app/sample-date-range-picker-app.ts
+++ src/app/sample-date-range-picker-app.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {SampleDateRangePickerNormal} from './components/sample-date-range-picker-normal/index';
-import {SampleDateRangePickerInline} from './components/sample-date-range-picker-inline/index';
 
 declare var require:any;
 const styles: string = require('./sample-date-range-picker-app.css');
@@ -8,9 +6,6 @@ const template: string = require('./sample-date-range-picker-app.html');
 
 @Component({
   selector: 'mydaterangepicker-app',
-  providers: [],
-  pipes: [],
-  directives: [SampleDateRangePickerNormal, SampleDateRangePickerInline],
   styles: [styles],
   template
 })
--- src/app/sample-date-range-picker-module.ts
+++ src/app/sample-date-range-picker-module.ts
@@ -1,15 +1,13 @@
-import {NgModule}      from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MyDateRangePickerApp}  from './sample-date-range-picker-app';
+import {SampleDateRangePickerNormal} from './components/sample-date-range-picker-normal/index';
+import {SampleDateRangePickerInline} from './components/sample-date-range-picker-inline/index';
+import {MyDateRangePicker} from './components/my-date-range-picker/index';
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [MyDateRangePickerApp],
-    bootstrap: [MyDateRangePickerApp]
+    imports:      [ BrowserModule ],
+    declarations: [ MyDateRangePickerApp, SampleDateRangePickerNormal, SampleDateRangePickerInline, MyDateRangePicker ],
+    bootstrap:    [ MyDateRangePickerApp ]
 })
-export class SampleDateRangePickerModule {
-
-    constructor() {
-        console.log('constructor: SampleDateRangePickerModule');
-    }
-}
+export class SampleDateRangePickerModule { }
