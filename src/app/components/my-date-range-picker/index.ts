20dc0ba40cd0f0c91d54525785e36ed550d30d20
kekeh <kea@live.fi>
2016-09-18 19:16:21 +0300

    Demo page app updated.

--- src/app/components/my-date-range-picker/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './my-date-range-picker.component';
-export * from './interfaces/index';
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,2 @@
 export * from './components/sample-date-range-picker-normal/index';
 export * from './components/sample-date-range-picker-inline/index';
-export * from './components/my-date-range-picker/index';
