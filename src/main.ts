d878c89fb35e77da52b9ca84e8ed6a2905d7912f
kekeh <kea@live.fi>
2016-09-21 22:21:48 +0300

    Demo page app updated.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+///<reference path="../node_modules/typescript/lib/lib.es6.d.ts"/>
+///<reference path="../node_modules/@types/node/index.d.ts"/>
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { SampleDateRangePickerModule } from './app/sample-date-range-picker-module';
 
