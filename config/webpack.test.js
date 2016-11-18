dd955331f24fbe2b06f969a67e98362865e8acef
kekeh <kea@live.fi>
2016-11-18 16:37:28 +0200

    Build package now contain template and styles as inline.

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -10,7 +10,8 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                loader: 'ts'
+                loader: 'ts',
+                exclude: [/npmdist/]
             },
             {
                 test: /\.html$/,
