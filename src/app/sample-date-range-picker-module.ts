b51a6532e3aeb8ccd2e190f26fb47bd98253177c
kekeh <kea@live.fi>
2016-12-08 16:23:54 +0200

    Demo page app updated.

--- src/app/sample-date-range-picker-module.ts
+++ src/app/sample-date-range-picker-module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {MyDateRangePickerApp}  from './sample-date-range-picker-app';
 import {SampleDateRangePickerNormal} from './components/sample-date-range-picker-normal/index';
 import {SampleDateRangePickerInline} from './components/sample-date-range-picker-inline/index';
-import {MyDateRangePickerModule} from 'mydaterangepicker/src/my-date-range-picker/my-date-range-picker.module';
+import {MyDateRangePickerModule} from 'mydaterangepicker';
 
 @NgModule({
     imports:      [ BrowserModule, MyDateRangePickerModule ],
