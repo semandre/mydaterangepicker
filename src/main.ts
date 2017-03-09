b3db961022fdecc0e40c2057171cf2024c632807
kekeh <kea@live.fi>
2017-03-09 20:13:19 +0200

    Demo page app updated.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { SampleDateRangePickerModule } from './app/sample-date-range-picker-module';
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(SampleDateRangePickerModule);
