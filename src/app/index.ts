f41058721d337699fddfc0ff85044dbbe373e598
kekeh <kea@live.fi>
2017-04-20 21:06:11 +0300

    Files deleted.

--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './components/sample-date-range-picker-normal/index';
-export * from './components/sample-date-range-picker-inline/index';
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { SampleDateRangePickerModule } from './app/sample-date-range-picker-module';
-
-enableProdMode();
-
-platformBrowserDynamic().bootstrapModule(SampleDateRangePickerModule);
